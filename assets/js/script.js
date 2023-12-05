e2b24c809644c5454baf73ff8a9c6065add37832
ruairi keane <ruairi.rk@icloud.com>
2023-12-05 15:15:23 +0000

    change html styling

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Declare constants for DOM elements
+ * and choices
+ */
+
+const buttons = document.getElementsByClassName("selector");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages")
+const choices = ['rock','paper','scissors'];
