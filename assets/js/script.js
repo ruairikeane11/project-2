abb914c5cfc0bb1eed23e45909ef5ee9b663a458
ruairi keane <ruairi.rk@icloud.com>
2023-12-08 18:03:44 +0000

    add playGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,49 @@ const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages")
-const choices = ['rock','paper','scissors'];
+const choices = ['rock', 'paper', 'scissors']
+
+let player;
+let computer;
+let result;
+
+
+
+/**
+ * Add event listeners to all the buttons
+ */
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerScore.textContent = `Player: ${player}`;
+    computerScore.textContent = `Computer: ${computer}`;
+    checkWinner();
+}));
+
+
+/**
+ * Main Game function, takes one parameter 
+ * which is the data-choice value of selected button
+ */
+
+function playGame(playerChoice) {
+    playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    computerChoice.src = `assets/images/${choices[computerChoice]}.jpg`
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
+
+/**
+ * Checks who the winner is, accepts 
+ */
+
+
